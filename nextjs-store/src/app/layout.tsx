dfd999f85c81d1f56021347b57ac82c66bdaa888
00lar <eduardani84@gmail.com>
2024-07-04 13:12:47 -0600

    image optimization and roboto added

--- nextjs-store/src/app/layout.tsx
+++ nextjs-store/src/app/layout.tsx
@@ -1,10 +1,11 @@
-import { Inter } from "next/font/google";
+import {Roboto} from "next/font/google"
 import { Header } from "./store/components/shared/Header";
 import { Footer } from "./store/components/shared/Footer/Footer";
 
-
-
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["100", "300", "500", "700"],
+  subsets: ["latin"],
+})
 
 export default function RootLayout({
   children,
@@ -14,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className="m-0 p-0 bg-primary-color text-text-color">
+      <body className={`m-0 p-0 bg-primary-color text-text-color ${roboto.className}`}>
       <Header/>
           {children}
         <Footer/>
