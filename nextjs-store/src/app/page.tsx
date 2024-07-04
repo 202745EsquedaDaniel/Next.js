5ea66df37663d72d5594530be0cd0d2eb0d8d451
00lar <eduardani84@gmail.com>
2024-07-03 22:44:41 -0600

    Architecture of landing page

--- nextjs-store/src/app/page.tsx
+++ nextjs-store/src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { Description } from "./store/components/home/Description";
+import { Hero } from "./store/components/home/Hero";
+import { MainProducts } from "./store/components/home/MainProducts";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hola mundo</h1>
+      <Hero />
+      <Description />
+      <MainProducts />
     </main>
   );
 }
---
+++ nextjs-store/src/app/store/components/shared/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <p>Future Worl</p>
+        </footer>
+    )
+}
