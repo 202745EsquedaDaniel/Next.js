cabe111ae0cdcd9e3fd34540d636339ce15618e0
00lar <eduardani84@gmail.com>
2024-07-06 15:14:36 -0600

    arquitectura profesional para fetching

--- nextjs-store/src/components/home/MainProducts/MainProducts.tsx
+++ nextjs-store/src/components/home/MainProducts/MainProducts.tsx
@@ -1,18 +1,6 @@
+import { getProducts } from "app/services/shopify"
 import Image from "next/image"
-
-const getProducts = async () => {
-    try {
-      const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
-        headers: new Headers({
-          'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
-        })
-      })
-      const { products } = await response.json()
-      return products
-    } catch (error) {
-      console.log(error)
-    }
-  }
+getProducts
 
 export const MainProducts = async () => {
     const products = await getProducts()
